dad7526737a2415f22b25a3e1af2bf31478a86f2
Denis Malinochkin <dmalinochkin@rambler.ru>
2015-11-02 23:46:00 +0300

    Pretty code

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,38 +10,38 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
-
-    // Configuration to be run (and then tested).
-    htmlhint: {
-      all: {
-        options: {
-          'tag-pair': true,
-          'htmlhintrc': 'test/.htmlhintrc'
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
         },
-        src: 'test/fixtures/*.html'
-      }
-    }
 
-  });
+        // Configuration to be run (and then tested).
+        htmlhint: {
+            all: {
+                options: {
+                    'tag-pair': true,
+                    'htmlhintrc': 'test/.htmlhintrc'
+                },
+                src: 'test/fixtures/*.html'
+            }
+        }
 
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
 
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'htmlhint']);
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // By default, lint and run all tests.
+    grunt.registerTask('default', ['jshint', 'htmlhint']);
 
 };
